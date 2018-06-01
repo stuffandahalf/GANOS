54db0c0a2852ee8a1c23824554a1d97556e0ac35
Gregory Norton <gregory.norton@me.com>
2018-05-31 23:01:48 -0400

    Added ega colours

--- include/video/vga.h
+++ include/video/vga.h
@@ -1,6 +1,23 @@
 #ifndef _VGA_H
 #define _VGA_H
 
+#define _VGA_COLOUR_BLACK       0x0
+#define _VGA_COLOUR_BLUE        0x1
+#define _VGA_COLOUR_GREEN       0x2
+#define _VGA_COLOUR_CYAN        0x3
+#define _VGA_COLOUR_RED         0x4
+#define _VGA_COLOUR_MAGENTA     0x5
+#define _VGA_COLOUR_BROWN       0x6
+#define _VGA_COLOUR_LTGREY      0x7
+#define _VGA_COLOUR_DRKGREY     0x8
+#define _VGA_COLOUR_LTBLUE      0x9
+#define _VGA_COLOUR_LTGREEN     0xA
+#define _VGA_COLOUR_LTCYAN      0xB
+#define _VGA_COLOUR_LTRED       0xC
+#define _VGA_COLOUR_LTMAGENTA   0xD
+#define _VGA_COLOUR_LTYELLOW    0xE
+#define _VGA_COLOUR_WHITE       0xF
+
 void init();
 
 #endif
