971fca4b203eaf49f98cf5c53e461c991324a14a
Gregory Norton <gregory.norton@me.com>
2018-08-29 09:22:07 -0400

    Added space for partition table, removed bios parameter block

--- boot/i386/boot0.asm
+++ boot/i386/boot0.asm
@@ -1,92 +1,15 @@
     [BITS 16]
     org 0x7c00
 
-jump:
-    jmp _start
-    nop
-oem:
-    db 'GANIX   '
-dos_2_0_bios_parameter_block:
-sector_size:
-    dw 512
-logical_sectors_per_cluster:
-    db 32
-reserved_logical_sector_count:
-    dw 18
-number_of_fats:
-    db 2
-number_of_root_dirs:
-    dw 224
-total_logical_sectors_2_0:
-    dw 2880     ; 3.5" 1.44MB
-media_descriptor:
-    db 0xF0
-logical_sectors_per_fat:
-    dw 9
-
-dos_3_31_bios_parameter_block:
-physical_sectors_per_track:
-    dw 18
-number_of_heads:
-    dw 2
-hidden_sectors_before_fat_count:
-    dd 0
-total_logical_sectors_3_31:
-    dd 0
-
-
-boot_str db `Ganix boot stage 0\r\n`, 0
+reserved_sectors: equ 18
 stage1_seg: equ 0000h
 stage1_offset: equ 1000h
-;a20_fail_str db `Failed to enable address line 20\r\n`, 0
 
 _start:
     mov [drive_num], dl     ; save loading drive number
 
     mov si, boot_str        ; load string into source index
     call print              ; and print it
-
-    call enable_a20
-
-    call load_boot1
-    jmp stage1_seg:stage1_offset
-
-load_boot1:
-.reset_disk:
-    mov ah, 00h
-    int 13h
-
-.load_sectors:
-    mov ax, stage1_seg
-    mov es, ax
-    mov bx, stage1_offset
-
-    mov ah, 2               ; function 2
-;    mov al, 1               ; number of sectors to read (1-128)
-    mov al, [reserved_logical_sector_count]
-    dec al
-    mov ch, 0               ; track/cylinder (0-1023)
-    mov cl, 2               ; starting sector (1-17)
-    mov dh, 0               ; head number (0-15)
-    mov dl, [drive_num]     ; restore drive number
-    int 13h
-
-    jc .retry
-
-    ret
-
-.retry:
-    dec byte [.drv_reset_counter] ; decrement reset timeout
-    jz .fail
-    jmp .reset_disk
-
-.fail:
-    mov si, .drv_fail_str
-    call print
-    jmp halt
-
-.drv_reset_counter: db 3
-.drv_fail_str: db `Failed to load stage 1 bootloader\r\n`
 
 enable_a20:
     call .check
@@ -192,14 +115,62 @@ enable_a20:
     call print
     jmp halt
 
+.fail_str: db `a20 failed\r\n`, 0
+.success_str: db `a20 enabled\r\n`, 0
+
 .exit:
     mov si, .success_str
     call print
 
-    ret
+load_gdt:
+    lgdt [gdt_info]
 
-.fail_str: db `Failed to enable address line 20 \r\n`, 0
-.success_str: db `Enabled address line 20\r\n`, 0
+load_boot1:
+.reset_disk:
+    mov ah, 00h
+    int 13h
+
+.load_sectors:
+    mov ax, stage1_seg
+    mov es, ax
+    mov bx, stage1_offset
+
+    mov ah, 2               ; function 2
+;    mov al, 1               ; number of sectors to read (1-128)
+    mov al, reserved_sectors
+    dec al
+    mov ch, 0               ; track/cylinder (0-1023)
+    mov cl, 2               ; starting sector (1-17)
+    mov dh, 0               ; head number (0-15)
+    mov dl, [drive_num]     ; restore drive number
+    int 13h
+
+    jc .retry
+
+    jmp .exit
+
+.retry:
+    dec byte [.drv_reset_counter] ; decrement reset timeout
+    jz .fail
+    jmp .reset_disk
+
+.fail:
+    mov si, .drv_fail_str
+    call print
+    jmp halt
+
+.drv_reset_counter: db 3
+;.drv_fail_str: db `Failed to load stage 1 bootloader\r\n`, 0
+;.drv_success_str: db `Loaded stage 1 bootloader\r\n`, 0
+.drv_fail_str: db `Loading failed\r\n`, 0
+.drv_success_str: db `Loaded stage 1\r\n`, 0
+
+.exit:
+    mov si, .drv_success_str
+    call print
+
+    jmp stage1_seg:stage1_offset
+
 
 ; prints the \0 terminated string located in si
 print:
@@ -209,7 +180,7 @@ print:
     lodsb
     cmp al, 0
     jz .end
-    int 10h
+    int 0x10
     jmp .loop
 .end:
     pop ax
@@ -218,10 +189,27 @@ print:
 halt:
     jmp halt
 
-    times 509 - ($ - $$) db 0
-
 drive_num:
     db 0
+
+boot_str: db `Ganix boot stage 0\r\n`, 0
+
+gdt_info:
+    dw gdt_end - gdt - 1
+    dd gdt
+
+gdt:
+    dd 0, 0
+flatdesc db 0xff, 0xff, 0, 0, 10010010b, 11001111b, 0
+gdt_end:
+
+    times 446 - ($ - $$) db 0
+
+part_tbl:
+.part1: times 16 db 0
+.part2: times 16 db 0
+.part3: times 16 db 0
+.part4: times 16 db 0
 
 boot_sig:
     db 0x55
--- boot/i386/boot1.asm
+++ boot/i386/boot1.asm
@@ -1,14 +1,38 @@
     [BITS 16]
     org 0x1000
 
-    mov si, msg
+STRUC MultibootHeader
+    .magic: resd 1
+    .flags: resd 1
+    .checksum: resd 1
+    .header_addr: resd 1
+    .load_addr: resd 1
+    .load_end_addr: resd 1
+    .bss_end_addr: resd 1
+    .entry_addr: resd 1
+    .mode_type: resd 1
+    .width: resd 1
+    .height: resd 1
+    .depth: resd 1
+    .size:
+ENDSTRUC
+
+_start:
+    mov si, boot_str
     call print
 
-halt:
-    nop
-    jmp halt
 
-msg: db `Ganix boot stage 1\r\n`, 0
+
+    cli
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    hlt
+
+
+boot_str: db `Ganix boot stage 1\r\n`, 0
 
 print:
     push ax
@@ -22,5 +46,8 @@ print:
 .end:
     pop ax
     ret
+
+halt:
+    jmp halt
 
     times 8704 - ($-$$) db 0
