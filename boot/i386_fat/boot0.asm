36c376c1b16818fa27f3e39927fb36274ad4acb4
Gregory Norton <gregory.norton@me.com>
2019-01-15 02:58:20 -0500

    Added initial fat bootloader

--- boot/i386_fat/boot0.asm
+++ boot/i386_fat/boot0.asm
@@ -1,20 +1,61 @@
     [BITS 16]
     org 0x7C00
-    
-;reserved_sectors: equ 18
 
-_start:
-    mov [drive_num], dl ;Preserve drive number of loading drive
+%define DEBUG
+
+start:
+    mov [data.drive_num], dl ;Preserve drive number of loading drive
+    
+    mov al, [part_tbl.part1 + part_entry.type]
+%ifndef DEBUG
+    cmp al, 0xEE
+    jnz halt
+%endif
+
+    mov si, data.test_str
+    call print
     
     
     
+    jmp halt
+
+print:
+    push ax
+    mov ah, 0x0E
+.loop:
+    lodsb
+    cmp al, 0
+    jz .end
+    int 0x10
+    jmp .loop
+.end:
+    pop ax
+    ret
+    
+halt:
+    jmp halt
+
+data:    
+.drive_num: db 0
+.test_str: db 'Hello World', 0
+
     times 446 - ($ - $$) db 0
 
+metadata:
+STRUC part_entry
+    .status: resb 1
+    .first_chs_address: resb 3
+    .type: resb 1
+    .last_chs_address: resb 3
+    .first_lba: resb 4
+    .sector_count: resb 4
+ENDSTRUC
+
 part_tbl:
-.part1: times 16 db 0
-.part2: times 16 db 0
-.part3: times 16 db 0
-.part4: times 16 db 0
+.part1: times part_entry_size db 0
+.part2: times part_entry_size db 0
+.part3: times part_entry_size db 0
+.part4: times part_entry_size db 0
 
 boot_sig:
     db 0x55
