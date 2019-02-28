8e8fb64e6663769ed1809be8ff5c9b40f85d90b2
Gregory Norton <gregory.norton@me.com>
2019-02-28 13:08:49 -0500

    Added fat32 root directory reading

--- tools/fat_reader/fat_reader.c
+++ tools/fat_reader/fat_reader.c
@@ -39,6 +39,24 @@ struct bpb {
     uint8_t boot_sig[2];
 } __attribute__((packed));
 
+struct dir_entry {
+    uint8_t lfn[32];
+    
+    uint8_t short_fname[8];
+    uint8_t short_ext[3];
+    uint8_t file_attributes;
+    uint8_t user_attributes;
+    uint8_t first_char_deleted_file;
+    uint16_t timestamp;
+    uint16_t creation_date;
+    uint16_t owner_id;
+    uint16_t first_cluster_high;
+    uint16_t last_modified_time;
+    uint16_t last_modified_date;
+    uint16_t first_cluster_low;
+    uint32_t file_size;
+} __attribute__((packed));
+
 int main(int argc, char **argv) {
     argc--; argv++;
     if (argc != 1) {
@@ -72,24 +90,47 @@ int main(int argc, char **argv) {
     }
     printf("\n");
 
-    printf("%ld\n", sizeof(struct bpb));
-    printf("%X\n", *(uint16_t *)bpb.boot_sig);
+    //printf("%X\n", *(uint16_t *)bpb.boot_sig);
     
-    printf("%X\n", bpb.root_dir_cluster);
+    printf("root directory cluster: %d\n", bpb.root_dir_cluster);
     
     
     size_t cluster_size = bpb.sectors_per_cluster * bpb.bytes_per_sector;
     printf("Cluster Size: %ld\n", cluster_size);
-    printf("offset: %d\n", (bpb.reserved_sectors * bpb.bytes_per_sector) + (bpb.root_dir_cluster * cluster_size));
-    fseek(dev, (bpb.reserved_sectors + (bpb.number_of_fats * bpb.sectors_per_fat)) * bpb.bytes_per_sector, SEEK_SET);
+    printf("reserved sectors %d\n", bpb.reserved_sectors);
+    printf("Sectors per FAT: %d\n", bpb.sectors_per_fat);
+    
+    size_t root_dir_offset = (bpb.reserved_sectors + (bpb.number_of_fats * bpb.sectors_per_fat)) * bpb.bytes_per_sector
+                           + (bpb.root_dir_cluster - 2) * cluster_size;
+    
+    printf("offset: %lX\n", root_dir_offset);
+    fseek(dev, root_dir_offset, SEEK_SET);
     uint8_t *root_cluster = malloc(sizeof(uint8_t) * cluster_size);
     if (root_cluster == NULL) {
         fprintf(stderr, "Failed to allocate memory for root directory cluster.\n");
         return 1;
     }
+    
     fread(root_cluster, sizeof(uint8_t), cluster_size, dev);
+    
+    printf("directory entry size: %lX\n", sizeof(struct dir_entry));
+    
+    struct dir_entry *entry = (struct dir_entry *)root_cluster;
+    for (int i = 0; i < cluster_size / sizeof(struct dir_entry); i++) {
+        printf("Size: %d\n", entry[i].file_size);
+        for (int j = 0; j < 8; j++) {
+            printf("%c", entry[i].short_fname[j]);
+        }
+        printf(".");
+        for (int j = 0; j < 3; j++) {
+            printf("%c", entry[i].short_ext[j]);
+        }
+        printf("\n");
+        //printf("fname: %s"
+    }
+    
     /*for(int i = 0; i < cluster_size; i++) {
-        printf("%d\n", root_cluster[i]);
+        printf("%X\n", root_cluster[i]);
     }*/
     
     // SUCCESS
