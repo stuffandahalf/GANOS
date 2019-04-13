53e78bac101bda31607b7ecbceb242e70131b3f2
Gregory Norton <gregory.norton@me.com>
2019-04-13 05:06:09 -0400

    Started stage 1 bootloader

--- boot/i386_fat/boot1.asm
+++ boot/i386_fat/boot1.asm
@@ -1,15 +1,127 @@
     [BITS 16]
     org 0x500
 
-    mov si, msg
+_start:
+    mov si, strings.start
     call print
-    jmp tst
 
-    times 1000 db 0
-tst:
-    mov si, msg
+enable_a20:
+    call .check
+    jnc .exit
+
+.enable_bios:
+    mov ax, 0x2401
+    int 0x15
+
+    call .check
+    jnc .exit
+
+.enable_kb:
+    call .kb_wait_com
+    mov al, 0xD1
+    out 0x64, al
+    call .kb_wait_com
+    mov al, 0xDF
+    out 0x60, al
+    call .kb_wait_com
+
+    call .check
+    jnc .exit
+
+.enable_fast:
+    in al, 0x92
+    test al, 2
+    jnz .enable_end
+    or al, 0x02
+    and al, 0xFE
+    out 0x92, al
+
+.enable_end:
+    call .check
+    jnc .exit
+    jmp .fail
+
+.kb_wait_com:
+    in al, 0x64
+    test al, 0x02
+    jnz .kb_wait_com
+    ret
+
+.kb_wait_data:
+    in al, 0x64
+    test al, 0x01
+    jnz .kb_wait_data
+    ret
+
+; routine from wiki.osdev.org
+.check:
+    pushf
+    push ds
+    push es
+    push di
+    push si
+
+    xor ax, ax ; ax = 0
+    mov es, ax
+
+    not ax ; ax = 0xFFFF
+    mov ds, ax
+
+    mov di, 0x0500
+    mov si, 0x0510
+
+    mov al, byte [es:di]
+    push ax
+
+    mov al, byte [ds:si]
+    push ax
+
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xFF
+
+    cmp byte [es:di], 0xFF
+
+    pop ax
+    mov byte [ds:si], al
+
+    pop ax
+    mov byte [es:di], al
+
+    stc
+    je .check_exit
+    clc
+
+.check_exit:
+    pop si
+    pop di
+    pop es
+    pop ds
+    popf
+
+    ret
+
+.fail:
+    mov si, .fail_str
     call print
     jmp halt
+
+.fail_str: db `a20 failed\r\n`, 0
+.success_str: db `a20 enabled\r\n`, 0
+
+.exit:
+    mov si, .success_str
+    call print
+
+    mov si, strings.msg
+    call print
+    
+load_gdt:
+    lgdt [gdtr]
+    
+halt:
+    cli
+    hlt
+    
     
 print:
     push ax
@@ -24,8 +136,17 @@ print:
     pop ax
     ret
 
-halt:
-    cli
-    hlt
+gdt:
+.entry0:
+    dw 0
+    dw 0
+.entry1:
+    
 
-msg: db 'Hello World!', 0x0A, 0x0D, 0
+gdtr:
+    .size: $ - gdt
+    .offset: gdt
+
+strings:
+.start: db 'Loaded stage 1', 0x0A, 0x0D, 0
+.msg: db 'Hello World!', 0x0A, 0x0D, 0
---
+++ boot/i386_fat/boot1.c
@@ -0,0 +1,46 @@
+__asm__(".code16");
+
+#include <stdint.h>
+
+void print(const char *str);
+
+void outb(uint8_t port, uint8_t data) {
+    __asm__(
+        "outb, %%al, %%dl\n"
+        :
+        : a(data), d(port)
+        :
+    )
+}
+
+void inb(uint8_t port) {
+    
+}
+
+void _start() {
+    print("Hello World");
+    
+    
+    
+    __asm__(
+        "cli\n"
+        "hlt\n"
+    );
+}
+
+void print(const char *str) {
+    __asm__(
+        "movd %%eax, %%esi\n"
+        "movb 0x0E, %%ah\n"
+        "0:\n"
+        "lodsb\n"
+        "testb %%al, %%al\n"
+        "je 1f\n"
+        "int 0x10\n"
+        "jmp 0b\n"
+        "1:\n"
+        : 
+        : "a (str)"
+        :
+    );
+}
