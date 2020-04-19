45e2c2f2d81943de3b47be9a44c3c06bc86fba47
Gregory Norton <Gregory.Norton@me.com>
2020-04-19 06:08:17 -0400

    normalized spaces to tabs in configure.sh

--- configure.sh
+++ configure.sh
@@ -92,46 +92,46 @@ if test -n "$NEW_TOOLCHAIN_PREFIX"; then
 	TOOLCHAIN_PREFIX=${NEW_TOOLCHAIN_PREFIX/#~\//$HOME\/}
 	echo "[$STAGE] Updated toolchain prefix ($TOOLCHAIN_PREFIX)"
 
-  STAGE=`expr $STAGE + 1`
-  VALID_TOOLCHAIN=false
-  while test $VALID_TOOLCHAIN != true; do
-    read -p "[$STAGE] Is the toolchain gcc or llvm " TOOLCHAIN_TYPE
-    case $TOOLCHAIN_TYPE in
-    gcc)
-      VALID_TOOLCHAIN=true
-      ;;
-    llvm)
-      VALID_TOOLCHAIN=true
-      ;;
-    *)
-      echo "Please enter one of gcc or llvm" 1>&2
-      ;;
-    esac
-  done
-  unset VALID_TOOLCHAIN
+	STAGE=`expr $STAGE + 1`
+	VALID_TOOLCHAIN=false
+	while test $VALID_TOOLCHAIN != true; do
+		read -p "[$STAGE] Is the toolchain gcc or llvm " TOOLCHAIN_TYPE
+		case $TOOLCHAIN_TYPE in
+		gcc)
+			VALID_TOOLCHAIN=true
+			;;
+		llvm)
+			VALID_TOOLCHAIN=true
+			;;
+		*)
+			echo "Please enter one of gcc or llvm" 1>&2
+			;;
+		esac
+	done
+	unset VALID_TOOLCHAIN
 
-  STAGE=`expr $STAGE + 1`
-  echo "[$STAGE] Detecting development tools"
-  case $TOOLCHAIN_TYPE in
-  gcc)
-    AS="$TOOLCHAIN_PREFIX/$ARCH-elf-as"
-    LD="$TOOLCHAIN_PREFIX/$ARCH-elf-ld"
-    CC="$TOOLCHAIN_PREFIX/$ARCH-elf-gcc"
-    ;;
-  llvm)
-    echo "LLVM toolchain is not currently supported" 1>&2
-    exit 1
-    ;;
-  esac
+	STAGE=`expr $STAGE + 1`
+	echo "[$STAGE] Detecting development tools"
+	case $TOOLCHAIN_TYPE in
+	gcc)
+		AS="$TOOLCHAIN_PREFIX/$ARCH-elf-as"
+		LD="$TOOLCHAIN_PREFIX/$ARCH-elf-ld"
+		CC="$TOOLCHAIN_PREFIX/$ARCH-elf-gcc"
+		;;
+	llvm)
+		echo "LLVM toolchain is not currently supported" 1>&2
+		exit 1
+		;;
+	esac
 
-  for TOOL in $AS $LD $CC; do
-    if test -f "$TOOL"; then
-      echo "[$STAGE] Found $TOOL"
-    else
-      echo "[$ERROR] $TOOL not found" 1>&2
-      exit 1
-    fi
-  done
+	for TOOL in $AS $LD $CC; do
+		if test -f "$TOOL"; then
+			echo "[$STAGE] Found $TOOL"
+		else
+			echo "[$ERROR] $TOOL not found" 1>&2
+			exit 1
+		fi
+	done
 fi
 
 STAGE=`expr $STAGE + 1`
@@ -141,10 +141,10 @@ echo "#This make include file was auto generated on `date`" >> "$OUT_FILE"
 echo "IS_GNU_MAKE=$IS_GNU_MAKE" >> "$OUT_FILE"
 echo "ARCH=$ARCH" >> "$OUT_FILE"
 if test -n "$NEW_TOOLSHAIN_PREFIX"; then
-  echo "TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX" >> "$OUT_FILE"
-  echo "AS=$AS" >> "$OUT_FILE"
-  echo "LD=$LD" >> "$OUT_FILE"
-  echo "CC=$CC" >> "$OUT_FILE"
+ 	echo "TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX" >> "$OUT_FILE"
+	echo "AS=$AS" >> "$OUT_FILE"
+	echo "LD=$LD" >> "$OUT_FILE"
+	echo "CC=$CC" >> "$OUT_FILE"
 fi
 
 STAGE=`expr $STAGE + 1`
