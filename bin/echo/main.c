861167e549c302f4b851e5f242209be7d82b888c
Gregory Norton <gregory.norton@me.com>
2020-05-01 22:50:25 -0400

    Added initial echo based on unix sysv version

---
+++ bin/echo/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+	int i;
+
+	argc--;
+	for (i = 1; i <= argc; i++) {
+		printf("%s%c", argv[i], i == argc ? '\n' : ' ');
+	}
+
+	return 0;
+}
