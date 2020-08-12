a2616f97bae603b643d9f7b2d8395bf611179658
Gregory Norton <Gregory.Norton@me.com>
2020-08-12 12:50:48 -0400

    Added cmake files for building kernel

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -2,7 +2,7 @@
 #set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR i386)
 
-set(TRIPLE i386-unknown-nonw-elf)
+set(TRIPLE i386-unknown-none-elf)
 #set(TOOLS ${CMAKE_CURRENT_LIST_DIR}/../tools)
 get_filename_component(TOOLS ${CMAKE_CURRENT_LIST_DIR}/../tools ABSOLUTE)
 
---
+++ sys/kern/kern_main.c
@@ -0,0 +1,5 @@
+void kern_main(int argc, char *argv[])
+{
+	
+}
+
