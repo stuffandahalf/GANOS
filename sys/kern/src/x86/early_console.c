29f4f29e60c18e09085ad9def993aefe747539d4
Gregory Norton <Gregory.Norton@me.com>
2020-10-02 10:00:16 -0400

    Updated early x86 console

--- sys/kern/src/x86/early_console.c
+++ sys/kern/src/x86/early_console.c
@@ -12,6 +12,37 @@ uint8_t *fb_ptr = (uint8_t *)0xB8000;
 uint16_t rows = 24;
 uint16_t cols = 80;
 
+uint16_t row = 0;
+uint16_t col = 0;
+
+static inline size_t
+fb_index(uint16_t x, uint16_t y)
+{
+	return y * cols * 2 + x * 2;
+}
+
+void
+put_char(char c)
+{
+	size_t i = fb_index(col, row);
+	uint16_t x, y;
+	fb_ptr[i] = c;
+	fb_ptr[i + 1] = 7;
+	col++;
+	if (col > cols) {
+		row++;
+		col = 0;
+	}
+	if (row > rows) {
+		for (y = 1; y < rows; y++) {
+			for (x = 0; x < cols; x++) {
+				fb_ptr[fb_index(x, y - 1)] = fb_ptr[fb_index(x, y)];
+				fb_ptr[fb_index(x, y - 1) + 1] = fb_ptr[fb_index(x, y) + 1];
+			}
+		}
+	}
+}
+
 void
 early_console_init(void)
 {
@@ -25,21 +56,32 @@ early_console_init(void)
 	early_console_clear();
 }
 
-void early_console_clear(void)
+void
+early_console_clear(void)
 {
 	int i;
 	for (i = 0; i < rows * cols * 2; i++) {
 		fb_ptr[i] = 0;
 	}
+
+	row = 0;
+	col = 0;
 }
 
 void
 early_console_print(const char *msg)
 {
 	const char *c;
+	uint16_t i, j;
 	for (c = msg; *c != '\0'; c++) {
-		*fb_ptr++ = *c;
-		*fb_ptr++ = 7;
+		switch (*c) {
+		default:
+			//*fb_ptr++ = *c;
+			//*fb_ptr++ = 7;
+			//col++;
+			put_char(*c);
+			break;
+		}
 		//*(fb_ptr++) = *(uint16_t *)&fbc;
 	}
 }
--- sys/kern/src/x86/init.c
+++ sys/kern/src/x86/init.c
@@ -4,10 +4,10 @@
 #include <platform/init.h>
 #if ALIX_KERNEL_FORMAT_MULTIBOOT
 #include <platform/x86/multiboot.h>
-extern struct multiboot_info *mb_info = NULL;
+extern struct multiboot_info *mb_info;
 #endif /* ALIX_KERNEL_FORMAT_MULTIBOOT */
 
-void *platform_info = (void *)0x0
+void *platform_info = (void *)0x0;
 
 // load memory map to platform_info
 void platform_init(void)
