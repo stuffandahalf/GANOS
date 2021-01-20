b70404a70046b0944cc5428ed211f4ebfdc63eca
Gregory Norton <gregory.norton@me.com>
2021-01-20 16:46:29 -0500

    Added fat formatting for gpt disk image

--- mkimg/i386/gpt/mkimg.sh
+++ mkimg/i386/gpt/mkimg.sh
@@ -22,11 +22,12 @@ dd if=/dev/zero of="$DEVICE" bs=512 count=`expr $SIZE / 512`
 sgdisk -o "$DEVICE"
 sgdisk -n 1:2048:1050624 -t 1:ef00 $DEVICE
 if [ ${DEVICE: -4} == ".img" ]; then
-	LOOP_DEV=`sudo losetup -P --fine --show $DEVICE`
+	LOOP_DEV=`sudo losetup -Pf --show $DEVICE`
 else
 	LOOP_DEV=$DEVICE
 fi
-sudo mkfs.fat -F32 $LOOP_DEVp1
-if [${DEVICE: -4} == ".img" ]; then
+sudo mkfs.fat -F32 `printf "%sp1" "$LOOP_DEV"`
+if [ ${DEVICE: -4} == ".img" ]; then
+	echo $LOOP_DEV
 	sudo losetup -d $LOOP_DEV
 fi
