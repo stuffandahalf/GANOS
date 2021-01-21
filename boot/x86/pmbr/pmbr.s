9620e07460aa968582bb934848935c0008c2399b
Gregory Norton <gregory.norton@me.com>
2021-01-20 20:05:19 -0500

    Added missing bootsector writing and updated partition table in pmbr to contain protective partition

--- boot/x86/pmbr/pmbr.s
+++ boot/x86/pmbr/pmbr.s
@@ -1,20 +1,32 @@
 	.code16
 	.globl _start
 
-	.set BUFFER, $0x500
+	.set PMBR_SZ, (bottom - top)
+	.set LOAD_ADDR, 0x7c00
+	.set LINK_ADDR, top
+	.set STACK_ADDR, 0x0600
+	.set BUFFER, 0x0600
 
 top:
 	xorw %ax, %ax
 	movw %ax, %ss
-	movw $top, %sp
+	movw $STACK_ADDR, %sp
 	pushw %es
 	pushw %di
+	pushw %dx
 	movw %ax, %es
+
+	movw $LOAD_ADDR, %si
+	movw $LINK_ADDR, %di
+	movw $PMBR_SZ, %cx
+	rep movsb
 	ljmp $0x0000, $_start
 
 _start:
+	sti
 	movw $str, %si
 	call print
+	jmp halt
 
 verify:
 	movb $0x41, %ah
@@ -59,7 +71,16 @@ ns_str:	.asciz "Not supported\r\n"
 
 	.org top+446
 
-part0:	.quad 0, 0
+#part0:	.quad 0, 0
+part0:
+	.byte 0x80
+	.byte 0x00
+	.word 0x0001
+	.byte 0xee
+	.byte 0xff
+	.word 0xffff
+	.long 0x00000001
+	.long 0xffffffff
 part1:	.quad 0, 0
 part2:	.quad 0, 0
 part3:	.quad 0, 0
